ef6584cdd57e5e5e3f502539da566c7087b4802f
telugujayaprakash <jayaprakash96030@gmail.com>
2024-04-14 12:46:04 +0530

    package errors resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css'
+import { Analytics } from "@vercel/analytics/react"
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
